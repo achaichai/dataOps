2fda5e15c7ae0c2ca8c0e3f0142f62c07cd7c8b4
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-04 16:33:07 -0500

    Added input parameters

--- examples/runGenerateData.sh
+++ examples/runGenerateData.sh
@@ -3,5 +3,14 @@
 # Add root directory to path
 PATH=$PATH:..
 
+# Input folder
+inputFolder=exampleData
+
+# Individual file size in MB
+fileSize=10
+
+# Subfolder names and sizes
+folders=location,64,sensors,138,devices,24
+
 # Generate data
-generateData.py --help
+generateData.py -i $inputFolder -s $fileSize -f $folders
