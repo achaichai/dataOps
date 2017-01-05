3506442c23df4070d3044846b1ca770acb7e4610
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-04 23:39:25 -0500

    Changed to reflect change in class names

--- dataOps/__init__.py
+++ dataOps/__init__.py
@@ -5,6 +5,6 @@ dataOps Library
 __author__ = 'James Chryssanthacopoulos'
 __version__ = '0.0.1'
 
-from dataOps import FileInitializer
-from dataOps import FileUpdater
-from dataOps import FileBackup
+from dataOps import DataInitializer
+from dataOps import DataUpdater
+from dataOps import DataBackup
