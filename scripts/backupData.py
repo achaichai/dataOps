af38619d36489bd0db0ea2dcb8c3e08ff732660b
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-04 23:37:39 -0500

    Changed to reflect change in class name

--- scripts/backupData.py
+++ scripts/backupData.py
@@ -6,6 +6,9 @@ Script to backup a master dataset
 
 The usage is:
     backupData.py -i <inputFolder> -b <backupFolder>
+where:
+    inputFolder:  location of existing master dataset,
+    backupFolder: location of backup folder
 
 """
 
@@ -16,9 +19,9 @@ __author__ = 'James Chryssanthacopoulos'
 
 
 # Define inputs
-parser = argparse.ArgumentParser(description = 'Script to backup data')
-parser.add_argument('-i', '--inputFolder', help = 'Name of input folder', required = True)
-parser.add_argument('-b', '--backupFolder', help = 'Name of backup folder', required = True)
+parser = argparse.ArgumentParser(description = 'Script to backup a master dataset')
+parser.add_argument('-i', '--inputFolder', help = 'Location of existing master dataset', required = True)
+parser.add_argument('-b', '--backupFolder', help = 'Location of backup folder', required = True)
 
 # Parse inputs
 args = parser.parse_args()
@@ -26,4 +29,4 @@ inputFolder = args.inputFolder
 backupFolder = args.backupFolder
 
 # Backup input folder
-dataOps.FileBackup().backup(inputFolder, backupFolder)
+dataOps.DataBackup().backup(inputFolder, backupFolder)
