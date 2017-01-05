085b83295d648f6c004b1058de17b32f3a087fac
James Chryssanthacopoulos <James@Jamess-MacBook-Air-2.local>
2017-01-05 00:52:56 -0500

    Added import of ControlledFileSystem baseclass

--- dataOps/__init__.py
+++ dataOps/__init__.py
@@ -5,6 +5,7 @@ dataOps Library
 __author__ = 'James Chryssanthacopoulos'
 __version__ = '0.0.1'
 
+from dataOps import ControlledFileSystem
 from dataOps import DataInitializer
 from dataOps import DataUpdater
 from dataOps import DataBackup
